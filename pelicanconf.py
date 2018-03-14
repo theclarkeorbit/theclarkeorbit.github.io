a325ed3170bb80a31f6713b9e2a675366c7f37e3
thegymnosophist <pbhogale@gmail.com>
2018-03-14 01:12:37 +0100

    yeehaw

--- pelicanconf.py
+++ pelicanconf.py
@@ -49,7 +49,7 @@ DEFAULT_DATE='fs'
 #THEME = "pelican-mockingbird"
 #THEME = "notmyidea"
 #THEME = "Flex"
-THEME = "pelican-blue"
+THEME = "pelican-fh5co-marble"
 
 MARKUP = 'md'
 #TYPOGRIFY = False
