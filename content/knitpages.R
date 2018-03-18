968b3f9dbb6bce316dbed10f4bcf065ba6a735c8
thegymnosophist <pbhogale@gmail.com>
2018-03-18 00:07:37 +0000

    yeehaw

--- content/knitpages.R
+++ content/knitpages.R
@@ -18,8 +18,8 @@ KnitPost <- function(input, outfile, figsfolder, cachefolder, base.url="/") {
   opts_chunk$set(fig.path = fig.path)
   opts_chunk$set(cache.path = cache.path)
   opts_chunk$set(fig.cap = "center")
-  opts_chunk$set(fig.width = 12)
-  opts_chunk$set(fig.height = 7)
+  opts_chunk$set(fig.width = 10)
+  opts_chunk$set(fig.height = 6)
   render_markdown()
   knit(input, outfile, envir = parent.frame())
 }
