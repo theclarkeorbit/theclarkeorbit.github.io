169375494bd85fdbca4a6d70b01aefe4d1f21d73
thegymnosophist <pbhogale@gmail.com>
2018-03-19 14:31:20 +0000

    yeehaw

--- pelicanconf.py
+++ pelicanconf.py
@@ -28,7 +28,7 @@ I18N_TEMPLATES_LANG = 'en'
 
 
 THEME = 'pelican-bootstrap3'
-
+GITHUB_USER = 'pbhogale'
 SITELOGO = u'https://en.gravatar.com/userimage/9352950/78ed70e67418f76f23b494458d53ac7d.jpg?size=400'
 
 FAVICON = SITEURL + "/images/favicon.png"
