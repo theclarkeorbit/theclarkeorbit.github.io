bd5399a69f8a785104f71c84aa52025e4c16791b
theclarkeorbit <prasanna.bhogale@gmail.com>
2024-04-27 15:34:24 +0530

    yeehaw

--- pelicanconf.py
+++ pelicanconf.py
@@ -27,7 +27,7 @@ I18N_TEMPLATES_LANG = 'en'
 #MENUITEMS = (('About', '/About.html'),)
 
 
-THEME = 'pelican-bootstrap2'
+THEME = '/home/prasanna/pelican-themes/pelican-bootstrap3'
 #GITHUB_USER = 'pbhogale'
 SITELOGO = "/images/gravtar.jpg"# SITEURL +  u'https://en.gravatar.com/userimage/9352950/78ed70e67418f76f23b494458d53ac7d.jpg?size=400'
 DISPLAY_RECENT_POSTS_ON_SIDEBAR = True
